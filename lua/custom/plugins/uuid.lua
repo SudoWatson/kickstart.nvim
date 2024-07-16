3b3a4da8e31be72d769cdf48ee8afca7ca46b07b
AustinLennertSW <135750082+AustinLennertSW@users.noreply.github.com>
2024-07-16 11:05:28 -0500

    Add uuid-nvim

---
+++ lua/custom/plugins/uuid.lua
@@ -0,0 +1,15 @@
+return {
+  'TrevorS/uuid-nvim',
+  lazy = false,
+  config = function()
+    -- optional configuration
+    local uuid = require('uuid-nvim')
+    uuid.setup{
+      case = 'upper',
+      quotes = 'none',
+      insert = 'before'
+    }
+    vim.keymap.set('i', '<C-g>', uuid.insert_v4, { desc = 'Insert [G]uid V4' })
+    -- vim.keymap.set('n', '<leader>g', 'i<C-g><Esc>', { desc = 'Insert [G]uid V4' })
+  end,
+}
