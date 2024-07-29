653f1f9501e7f0a576f946cfa03f516239cc7999
SudoWatson <aulennert@protonmail.com>
2024-07-29 14:47:37 -0500

    Leap

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -14,7 +14,7 @@ return { -- Collection of various small independent plugins/modules
     -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
     -- - sd'   - [S]urround [D]elete [']quotes
     -- - sr)'  - [S]urround [R]eplace [)] [']
-    require('mini.surround').setup()
+    -- TODO: Find a remapping for this since leap takes the S now => require('mini.surround').setup()
 
     -- Simple and easy statusline.
     --  You could remove this setup call if you don't like it,
